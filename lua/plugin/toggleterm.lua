ac0db9cf018dfe1db10fcaa50e11968aafb99f0e
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-29 19:53:08 -0600

    fix(toggleterm): fixed mappings and lazy-loading
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -1,5 +1,7 @@
 ---@module 'lazy'
 
+local floor = math.floor
+
 local function set_terminal_keymaps(ev) ---@param ev vim.api.keyset.create_autocmd.callback_args
     if not ev then
         return
@@ -42,7 +44,6 @@ end
 ---@type LazySpec
 return {
     'akinsho/toggleterm.nvim',
-    lazy = true,
     version = false,
     keys = {
         {
@@ -54,65 +55,53 @@ return {
         },
     },
     enabled = not require('user_api.check').in_console(),
-    opts = {
-        size = function(term) ---@param term Terminal
-            return term.direction == 'vertical' and math.floor(vim.o.columns * 0.65)
-                or (function()
-                    return math.floor(vim.o.columns * 0.85)
-                end)()
-        end,
-        open_mapping = [[<A-t>]],
-        autochdir = true,
-        hide_numbers = true,
-        direction = 'float', ---@type 'float'|'tab'|'horizontal'|'vertical'
-        close_on_exit = true,
-        opts = {
-            border = 'rounded',
-            title_pos = 'center',
-            width = (function()
-                return math.floor(vim.o.columns * 0.85)
-            end)(),
-        },
-        highlights = {
-            Normal = { guibg = '#291d3f' },
-            NormalFloat = { link = 'Normal' },
-            FloatBorder = { guifg = '#c5c7a1', guibg = '#21443d' },
-        },
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = -30,
-        shading_ratio = -3,
-        start_in_insert = true,
-        insert_mappings = true,
-        terminal_mappings = true,
-        shell = vim.o.shell,
-        auto_scroll = true,
-        persist_size = true,
-        persist_mode = true,
-        float_opts = {
-            border = 'curved',
-            title_pos = 'center',
-            zindex = 100,
-            winblend = 3,
-        },
-        winbar = {
-            enabled = true,
-            name_formatter = function(term) ---@param term Terminal
-                return term.name
+    config = function()
+        require('toggleterm').setup({
+            size = function(term) ---@param term Terminal
+                local cols = vim.o.columns
+                return term.direction == 'vertical' and floor(cols * 0.65) or floor(cols * 0.85)
             end,
-        },
-    },
-    config = function(_, opts)
-        require('toggleterm').setup(opts)
-        local aus = { ---@type AuDict
-            TermOpen = {
-                group = vim.api.nvim_create_augroup('ToggleTerm.Hooks', { clear = false }),
-                callback = set_terminal_keymaps,
+            open_mapping = [[<A-t>]],
+            autochdir = true,
+            hide_numbers = true,
+            direction = 'float',
+            close_on_exit = true,
+            opts = { border = 'rounded', title_pos = 'center', width = floor(vim.o.columns * 0.85) },
+            highlights = {
+                Normal = { guibg = '#291d3f' },
+                NormalFloat = { link = 'Normal' },
+                FloatBorder = { guifg = '#c5c7a1', guibg = '#21443d' },
             },
-        }
-        for event, v in next, aus do
-            vim.api.nvim_create_autocmd(event, v)
-        end
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = -30,
+            shading_ratio = -3,
+            start_in_insert = true,
+            insert_mappings = true,
+            terminal_mappings = true,
+            shell = vim.o.shell,
+            auto_scroll = true,
+            persist_size = true,
+            persist_mode = true,
+            float_opts = { border = 'curved', title_pos = 'center', zindex = 100, winblend = 3 },
+            winbar = {
+                enabled = true,
+                name_formatter = function(term) ---@param term Terminal
+                    return term.name
+                end,
+            },
+        })
+        local group = vim.api.nvim_create_augroup('ToggleTerm.Hooks', { clear = true })
+        require('user_api.util.autocmd').au_from_dict({
+            TermOpen = { group = group, callback = set_terminal_keymaps },
+        })
+
+        local desc = require('user_api.maps').desc
+        require('user_api.config').keymaps({
+            n = {
+                ['<leader><A-t>'] = { '<CMD>exe v:count1 . "ToggleTerm"<CR>', desc('Toggleterm') },
+            },
+        })
     end,
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
