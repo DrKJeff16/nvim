d6b51ed4be5a66bdd884149ded63c37eb64d7ab0
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-31 00:59:34 -0600

    fix(yanky)!: reworked `yanky` config
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/yanky.lua
+++ lua/plugin/yanky.lua
@@ -4,29 +4,25 @@
 return {
     'gbprod/yanky.nvim',
     version = false,
-    dependencies = { 'kkharji/sqlite.lua', 'nvim-telescope/telescope.nvim' },
-    opts = {
-        ring = {
-            history_length = 100,
-            storage = 'shada',
-            storage_path = vim.fn.stdpath('data') .. '/databases/yanky.db',
-            sync_with_numbered_registers = true,
-            cancel_event = 'update',
-            ignore_registers = { '_' },
-            update_register_on_cycle = false,
-            permanent_wrapper = nil,
-        },
-        picker = {
-            select = { action = nil }, -- nil to use default put action
-            telescope = { use_default_mappings = true }, -- if default mappings should be used
-        },
-        system_clipboard = { sync_with_ring = true, clipboard_register = nil },
-        highlight = { on_put = true, on_yank = true, timer = 200 },
-        preserve_cursor_position = { enabled = true },
-        textobj = { enabled = true },
-    },
-    config = function(_, opts)
-        require('yanky').setup(opts)
+    dependencies = { 'kkharji/sqlite.lua', 'nvim-telescope/telescope.nvim', 'folke/snacks.nvim' },
+    config = function()
+        require('yanky').setup({
+            ring = {
+                history_length = 100,
+                storage = 'shada',
+                storage_path = vim.fn.stdpath('data') .. '/databases/yanky.db',
+                sync_with_numbered_registers = true,
+                cancel_event = 'update',
+                ignore_registers = { '_' },
+                update_register_on_cycle = false,
+                permanent_wrapper = nil,
+            },
+            picker = { select = { action = nil }, telescope = { use_default_mappings = true } },
+            system_clipboard = { sync_with_ring = true, clipboard_register = nil },
+            highlight = { on_put = true, on_yank = true, timer = 500 },
+            preserve_cursor_position = { enabled = true },
+            textobj = { enabled = true },
+        })
         require('telescope').load_extension('yank_history')
 
         vim.keymap.set({ 'o', 'x' }, 'iy', function()
@@ -34,13 +30,14 @@ return {
         end, {})
     end,
     keys = {
-        {
-            '<leader>P',
-            '<CMD>YankyRingHistory<CR>',
-            mode = { 'n', 'x' },
-            desc = 'Open Yank History',
-        },
-        { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank text' },
+        { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank Text' },
+        { '>p', '<Plug>(YankyPutIndentAfterShiftRight)', desc = 'Put and indent right' },
+        { '<p', '<Plug>(YankyPutIndentAfterShiftLeft)', desc = 'Put and indent left' },
+        { '>P', '<Plug>(YankyPutIndentBeforeShiftRight)', desc = 'Put before and indent right' },
+        { '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)', desc = 'Put before and indent left' },
+        { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put after applying a filter' },
+        { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put before applying a filter' },
+        { '<C-n>', '<Plug>(YankyNextEntry)', desc = 'Select next entry through yank history' },
         {
             'p',
             '<Plug>(YankyPutAfter)',
@@ -66,11 +63,10 @@ return {
             desc = 'Put yanked text before selection',
         },
         {
-            '<c-p>',
+            '<C-p>',
             '<Plug>(YankyPreviousEntry)',
             desc = 'Select previous entry through yank history',
         },
-        { '<c-n>', '<Plug>(YankyNextEntry)', desc = 'Select next entry through yank history' },
         {
             ']p',
             '<Plug>(YankyPutIndentAfterLinewise)',
@@ -91,12 +87,6 @@ return {
             '<Plug>(YankyPutIndentBeforeLinewise)',
             desc = 'Put indented before cursor (linewise)',
         },
-        { '>p', '<Plug>(YankyPutIndentAfterShiftRight)', desc = 'Put and indent right' },
-        { '<p', '<Plug>(YankyPutIndentAfterShiftLeft)', desc = 'Put and indent left' },
-        { '>P', '<Plug>(YankyPutIndentBeforeShiftRight)', desc = 'Put before and indent right' },
-        { '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)', desc = 'Put before and indent left' },
-        { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put after applying a filter' },
-        { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put before applying a filter' },
     },
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
