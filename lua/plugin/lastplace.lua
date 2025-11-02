0f3446bc4751e9a92ba46d23fcaa86611356e873
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-11-02 17:39:02 -0600

    fix(lastplace): update link and module
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lastplace.lua
+++ lua/plugin/lastplace.lua
@@ -2,11 +2,11 @@
 
 ---@type LazySpec
 return {
-    'DrKJeff16/nvim-lastplace',
+    'DrKJeff16/lastplace.nvim',
     dev = true,
     version = false,
     config = function()
-        require('nvim-lastplace').setup()
+        require('lastplace').setup()
     end,
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
