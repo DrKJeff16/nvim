6739e4c772de994f2c733fdf44e3a0c1e8da629a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-11-09 11:21:42 -0600

    chore(blink-indent): remove custom options
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/blink-indent.lua
+++ lua/plugin/blink-indent.lua
@@ -7,33 +7,6 @@ return {
     event = 'VeryLazy', -- WARN: VERY IMPORTANT
     version = false,
     cond = not require('user_api.check').in_console(),
-    config = function()
-        require('blink.indent').setup({
-            blocked = {
-                buftypes = { include_defaults = true },
-                filetypes = { include_defaults = true },
-            },
-            static = {
-                enabled = true,
-                char = '▎',
-                priority = 1,
-                highlights = { 'BlinkIndentCyan', 'BlinkIndentGreen', 'BlinkIndentBlue' },
-            },
-            scope = {
-                enabled = true,
-                char = '▎',
-                priority = 1000,
-                highlights = { 'BlinkIndentRed', 'BlinkIndentYellow', 'BlinkIndentOrange' },
-                underline = {
-                    enabled = true,
-                    highlights = {
-                        'BlinkIndentOrangeUnderline',
-                        'BlinkIndentVioletUnderline',
-                        'BlinkIndentBlueUnderline',
-                    },
-                },
-            },
-        })
-    end,
+    opts = {},
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
