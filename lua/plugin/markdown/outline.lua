12225785f87ca415722a9f1ead402d80949567ad
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-11-10 21:35:21 -0600

    feat(md-outline): added `md-outline.nvim`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ lua/plugin/markdown/outline.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+
+---@type LazySpec
+return {
+    'tttol/md-outline.nvim',
+    ft = { 'markdown' },
+    version = false,
+    opts = {},
+}
+--- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
