3f728e245677445a146d8149e7c2040d5e828d90
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-11-21 21:18:33 -0600

    fix(lastplace): use superior version
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lastplace.lua
+++ lua/plugin/lastplace.lua
@@ -1,12 +1,26 @@
 ---@module 'lazy'
 
----@type LazySpec
-return {
-    'DrKJeff16/lastplace.nvim',
-    dev = true,
+return { ---@type LazySpec
+    'nxhung2304/lastplace.nvim',
     version = false,
     config = function()
-        require('lastplace').setup()
+        require('lastplace').setup({
+            ignore_filetypes = {
+                'gitcommit',
+                'gitrebase',
+                'svn',
+                'hgcommit',
+                'xxd',
+                'COMMIT_EDITMSG',
+            },
+            ignore_buftypes = { 'quickfix', 'nofile', 'help', 'terminal' },
+            center_on_jump = true,
+            jump_only_if_not_visible = false,
+            min_lines = 10,
+            max_line = 0,
+            open_folds = true,
+            debug = false,
+        })
     end,
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
