661f3b22ec36a1980e21caf59a1133a8190815a7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-12-03 23:12:20 -0600

    chore(luaref): config tweaks
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/luaref.lua
+++ lua/plugin/luaref.lua
@@ -1,4 +1,8 @@
 ---@module 'lazy'
 
-return { 'emiasims/nvim-luaref', lazy = false, version = false } ---@type LazySpec
+return { ---@type LazySpec
+    'emiasims/nvim-luaref',
+    lazy = false,
+    version = false,
+}
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
