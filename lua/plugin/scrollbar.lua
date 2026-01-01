206f8e1f29d23ea8ad1a5b93af2abea9ccc90b99
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-01 14:34:05 -0600

    fix(scrollbar): update GitHub URL and config
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/scrollbar.lua
+++ lua/plugin/scrollbar.lua
@@ -1,27 +1,10 @@
 ---@module 'lazy'
-
 return { ---@type LazySpec
-  'wsdjeg/scrollbar.vim',
+  'wsdjeg/scrollbar.nvim',
   version = false,
   cond = not require('user_api.check').in_console(),
   config = function()
-    require('scrollbar').setup({
-      max_size = 10,
-      min_size = 5,
-      width = 1,
-      right_offset = 1,
-      shape = { head = '▲', body = '█', tail = '▼' },
-      highlight = { head = 'Normal', body = 'Normal', tail = 'Normal' },
-      excluded_filetypes = {
-        'TelescopePrompt',
-        'notify',
-        'startify',
-        'NvimTree',
-        'ministarter',
-        'neo-tree',
-        'lazy',
-      },
-    })
+    require('scrollbar').setup()
   end,
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
