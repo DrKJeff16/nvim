33e824ca8f268a59accfddf37706652e202a68d6
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-06 19:34:54 -0600

    chore(music-player): add condition to setup
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/music-player.lua
+++ lua/plugin/music-player.lua
@@ -1,8 +1,11 @@
 ---@module 'lazy'
+
+local Check = require('user_api.check')
 return { ---@type LazySpec
   'ricmonmol/nvim-music-player',
   dev = true,
   version = false,
   build = ':UpdateRemotePlugins',
+  cond = not (Check.in_console() or Check.is_root()),
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
