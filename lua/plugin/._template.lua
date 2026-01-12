c9af6b66f8a80c2d404eb8c6363cd888aa3df570
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-12 16:12:01 -0600

    chore: add plugin config template
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ lua/plugin/._template.lua
@@ -0,0 +1,9 @@
+---@module 'lazy'
+return { ---@type LazySpec
+  'username/plugin',
+  event = 'VeryLazy',
+  version = false,
+  dependencies = {},
+  opts = {},
+}
+-- vim: set ts=2 sts=2 sw=2 et ai si sta:
