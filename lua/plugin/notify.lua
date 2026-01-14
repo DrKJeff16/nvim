65e6e65063f905c9604d47f2511b88fd907a32fe
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-14 12:38:32 -0600

    fix(notify): update notify colors on colorscheme switch
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/notify.lua
+++ lua/plugin/notify.lua
@@ -1,4 +1,34 @@
 ---@module 'lazy'
+
+local set_hl = vim.schedule_wrap(function()
+  require('user_api.highlight').hl_from_dict({
+    NotifyDEBUGBody = { link = 'Normal' },
+    NotifyDEBUGBorder = { fg = '#CBCB42' },
+    NotifyDEBUGIcon = { fg = '#909042' },
+    NotifyDEBUGTitle = { fg = '#CBCB42' },
+    NotifyERRORBody = { link = 'ErrorMsg' },
+    NotifyERRORBorder = { fg = '#8A1F1F' },
+    NotifyERRORIcon = { fg = '#F70067' },
+    NotifyERRORTitle = { fg = '#F70067' },
+    NotifyINFOBody = { link = 'Normal' },
+    NotifyINFOBorder = { fg = '#4F6752' },
+    NotifyINFOIcon = { fg = '#A9FF68' },
+    NotifyINFOTitle = { fg = '#A9FF68' },
+    NotifyLOGBody = { link = 'Normal' },
+    NotifyLOGBorder = { fg = '#3F6072' },
+    NotifyLOGIcon = { fg = '#59BFAB' },
+    NotifyLOGTitle = { fg = '#59BFAB' },
+    NotifyTRACEBody = { link = 'Normal' },
+    NotifyTRACEBorder = { fg = '#4F3552' },
+    NotifyTRACEIcon = { fg = '#D484FF' },
+    NotifyTRACETitle = { fg = '#D484FF' },
+    NotifyWARNBody = { link = 'WarningMsg' },
+    NotifyWARNBorder = { fg = '#79491D' },
+    NotifyWARNIcon = { fg = '#F79000' },
+    NotifyWARNTitle = { fg = '#F79000' },
+  })
+end)
+
 return { ---@type LazySpec
   'rcarriga/nvim-notify',
   priority = 1000,
@@ -34,34 +64,14 @@ return { ---@type LazySpec
       })
     end
 
-    vim.schedule(function()
-      require('user_api.highlight').hl_from_dict({
-        NotifyDEBUGBody = { link = 'Normal' },
-        NotifyDEBUGBorder = { fg = '#CBCB42' },
-        NotifyDEBUGIcon = { fg = '#909042' },
-        NotifyDEBUGTitle = { fg = '#CBCB42' },
-        NotifyERRORBody = { link = 'ErrorMsg' },
-        NotifyERRORBorder = { fg = '#8A1F1F' },
-        NotifyERRORIcon = { fg = '#F70067' },
-        NotifyERRORTitle = { fg = '#F70067' },
-        NotifyINFOBody = { link = 'Normal' },
-        NotifyINFOBorder = { fg = '#4F6752' },
-        NotifyINFOIcon = { fg = '#A9FF68' },
-        NotifyINFOTitle = { fg = '#A9FF68' },
-        NotifyLOGBody = { link = 'Normal' },
-        NotifyLOGBorder = { fg = '#3F6072' },
-        NotifyLOGIcon = { fg = '#59BFAB' },
-        NotifyLOGTitle = { fg = '#59BFAB' },
-        NotifyTRACEBody = { link = 'Normal' },
-        NotifyTRACEBorder = { fg = '#4F3552' },
-        NotifyTRACEIcon = { fg = '#D484FF' },
-        NotifyTRACETitle = { fg = '#D484FF' },
-        NotifyWARNBody = { link = 'WarningMsg' },
-        NotifyWARNBorder = { fg = '#79491D' },
-        NotifyWARNIcon = { fg = '#F79000' },
-        NotifyWARNTitle = { fg = '#F79000' },
-      })
-    end)
+    set_hl()
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('NotifyHl', { clear = true }),
+      callback = function()
+        set_hl()
+      end,
+    })
   end,
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
