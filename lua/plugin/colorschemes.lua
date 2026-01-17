ab8b67941bfb139f1f0bee6f8ee5b99ab31261d4
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-17 15:06:42 -0600

    chore(color): maintenance
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/colorschemes.lua
+++ lua/plugin/colorschemes.lua
@@ -1,4 +1,5 @@
----@param color? string
+---@param color string|nil
+---@overload fun()
 return function(color)
   if vim.fn.has('nvim-0.11') == 1 then
     vim.validate('color', color, { 'string', 'nil' }, true)
@@ -8,8 +9,10 @@ return function(color)
   color = color or 'tokyonight'
   color = color:lower()
 
-  if not vim.list_contains(vim.fn.getcompletion('', 'color'), color) then
-    color = 'tokyonight'
+  local colors = vim.fn.getcompletion('', 'color') ---@type string[]
+  if not vim.list_contains(colors, color) then
+    color = require('user_api.check.exists').module('tokyonight') and 'tokyonight'
+      or colors[math.random(1, #colors)]
   end
 
   vim.cmd.colorscheme(color)
