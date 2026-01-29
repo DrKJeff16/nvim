395003637c87da70858aad83659aad6fb6b8f330
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-29 09:10:17 -0600

    fix(lspkind): shorten setup options
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lspkind.lua
+++ lua/plugin/lspkind.lua
@@ -1,40 +1,11 @@
 ---@module 'lazy'
 return { ---@type LazySpec
   'onsails/lspkind.nvim',
-  lazy = false,
+  dev = true,
+  lazy = true,
   version = false,
   config = function()
-    require('lspkind').init({
-      mode = 'symbol_text',
-      preset = 'codicons',
-      symbol_map = {
-        Text = '󰉿',
-        Method = '󰆧',
-        Function = '󰊕',
-        Constructor = '',
-        Field = '󰜢',
-        Variable = '󰀫',
-        Class = '󰠱',
-        Interface = '',
-        Module = '',
-        Property = '󰜢',
-        Unit = '󰑭',
-        Value = '󰎠',
-        Enum = '',
-        Keyword = '󰌋',
-        Snippet = '',
-        Color = '󰏘',
-        File = '󰈙',
-        Reference = '󰈇',
-        Folder = '󰉋',
-        EnumMember = '',
-        Constant = '󰏿',
-        Struct = '󰙅',
-        Event = '',
-        Operator = '󰆕',
-        TypeParameter = '',
-      },
-    })
+    require('lspkind').init({ mode = 'symbol_text', preset = 'default' })
   end,
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
