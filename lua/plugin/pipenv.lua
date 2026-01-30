9afcc964edfbf627805f989743f337f54015cae6
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-30 17:18:02 -0600

    fix(pipenv): update setup options
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/pipenv.lua
+++ lua/plugin/pipenv.lua
@@ -3,6 +3,8 @@ return { ---@type LazySpec
   'DrKJeff16/pipenv.nvim',
   dev = true,
   version = false,
-  opts = {},
+  config = function()
+    require('pipenv').setup({ output = { height = 0.75, width = 0.85 } })
+  end,
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
