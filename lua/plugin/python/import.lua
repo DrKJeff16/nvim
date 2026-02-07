ce2a6974b7531a9b550ac8e1ff8ac604cebad896
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-02-07 11:32:39 -0600

    fix(python.import): improve keymaps
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/python/import.lua
+++ lua/plugin/python/import.lua
@@ -2,59 +2,49 @@
 return { ---@type LazySpec
   'kiyoon/python-import.nvim',
   version = false,
+  cond = require('user_api.check.exists').executable('uv'),
   build = 'uv tool install . --force --reinstall',
   ft = 'python',
-  keys = {
-    {
-      '<M-CR>',
-      function()
-        require('python_import.api').add_import_current_word_and_notify()
+  config = function()
+    require('python_import').setup({})
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('PythonImport', { clear = true }),
+      pattern = { 'python' },
+      callback = function(ev)
+        local Api = require('python_import.api')
+        local desc = require('user_api.maps').desc
+        require('user_api.config.keymaps').set({
+          n = {
+            ['<M-\\>'] = {
+              Api.add_import_current_word_and_notify,
+              desc('Add Python Import', true, ev.buf),
+            },
+            ['<leader>i'] = { group = 'Python Import', buffer = ev.buf },
+            ['<leader>it'] = { Api.add_rich_traceback, desc('Add Rich Traceback', true, ev.buf) },
+            ['<leader>ii'] = {
+              Api.add_import_current_word_and_move_cursor,
+              desc('Add python import and move cursor', true, ev.buf),
+            },
+          },
+          i = {
+            ['<M-\\>'] = {
+              Api.add_import_current_word_and_notify,
+              desc('Add Python Import', true, ev.buf),
+            },
+          },
+          x = {
+            ['<leader>i'] = {
+              Api.add_import_current_selection_and_move_cursor,
+              desc('Add Python Import and Move Cursor', true, ev.buf),
+            },
+            ['<M-\\>'] = {
+              Api.add_import_current_selection_and_notify,
+              desc('Add Python Import', true, ev.buf),
+            },
+          },
+        }, ev.buf)
       end,
-      mode = { 'i', 'n' },
-      silent = true,
-      desc = 'Add python import',
-      ft = 'python',
-    },
-    {
-      '<M-CR>',
-      function()
-        require('python_import.api').add_import_current_selection_and_notify()
-      end,
-      mode = 'x',
-      silent = true,
-      desc = 'Add python import',
-      ft = 'python',
-    },
-    {
-      '<leader>i',
-      function()
-        require('python_import.api').add_import_current_word_and_move_cursor()
-      end,
-      mode = 'n',
-      silent = true,
-      desc = 'Add python import and move cursor',
-      ft = 'python',
-    },
-    {
-      '<leader>i',
-      function()
-        require('python_import.api').add_import_current_selection_and_move_cursor()
-      end,
-      mode = 'x',
-      silent = true,
-      desc = 'Add python import and move cursor',
-      ft = 'python',
-    },
-    {
-      '<leader>tr',
-      function()
-        require('python_import.api').add_rich_traceback()
-      end,
-      silent = true,
-      desc = 'Add rich traceback',
-      ft = 'python',
-    },
-  },
-  opts = {},
+    })
+  end,
 }
 -- vim: set ts=2 sts=2 sw=2 et ai si sta:
