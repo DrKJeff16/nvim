7b77248e40ae2771f0b0c796c07b3c23704d49f2
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-02-11 01:55:52 -0600

    fix(project): update keymaps
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/project.lua
+++ lua/plugin/project.lua
@@ -39,19 +39,40 @@ return { ---@type LazySpec
         ['<leader>pH'] = { vim.cmd.ProjectHealth, desc('Run `:checkhealth project`') },
         ['<leader>pa'] = { vim.cmd.ProjectAdd, desc('Add New Project') },
         ['<leader>pd'] = { vim.cmd.ProjectDelete, desc('Delete Existing Project') },
-        ['<leader>pf'] = { vim.cmd.ProjectFzf, desc('Run Fzf-Lua') },
         ['<leader>ph'] = { vim.cmd.ProjectHistory, desc('Toggle History Window') },
         ['<leader>pl'] = { vim.cmd.ProjectLog, desc('Toggle Log Window') },
         ['<leader>pp'] = { vim.cmd.Project, desc('Open Project UI') },
-        ['<leader>pr'] = { vim.cmd.ProjectRecents, desc('Select Recent Project') },
-        ['<leader>ps'] = { vim.cmd.ProjectSession, desc('Select From Current Session') },
+        ['<leader>pr'] = { vim.cmd.ProjectRecents, desc('Recent Projects') },
+        ['<leader>ps'] = { vim.cmd.ProjectSession, desc('Session') },
       },
     })
 
-    if require('user_api.check.exists').module('telescope') then
+    if
+      require('user_api.check.exists').module('fzf-lua')
+      and vim.cmd.ProjectFzf
+      and vim.is_callable(vim.cmd.ProjectFzf)
+    then
+      require('user_api.config').keymaps.set({
+        n = { ['<leader>pf'] = { vim.cmd.ProjectFzf, desc('Fzf-Lua Picker') } },
+      })
+    end
+    if
+      require('user_api.check.exists').module('telescope')
+      and vim.cmd.ProjectTelescope
+      and vim.is_callable(vim.cmd.ProjectTelescope)
+    then
       require('telescope').load_extension('projects')
       require('user_api.config').keymaps.set({
-        n = { ['<leader>pT'] = { vim.cmd.ProjectTelescope, desc('Open Telescope Picker') } },
+        n = { ['<leader>pT'] = { vim.cmd.ProjectTelescope, desc('Telescope Picker') } },
+      })
+    end
+    if
+      require('user_api.check.exists').module('snacks')
+      and vim.cmd.ProjectSnacks
+      and vim.is_callable(vim.cmd.ProjectSnacks)
+    then
+      require('user_api.config').keymaps.set({
+        n = { ['<leader>pS'] = { vim.cmd.ProjectSnacks, desc('Snacks Picker') } },
       })
     end
   end,
