4f14b57203688f584e88483a45b2930419514128
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-05-06 00:40:18 -0600

    fix: maintenance
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/config/lsp/servers/ruby_lsp.lua
+++ lua/config/lsp/servers/ruby_lsp.lua
@@ -9,6 +9,8 @@ return { ---@type vim.lsp.ClientConfig
   filetypes = { 'ruby', 'eruby' },
   root_markers = { 'Gemfile', '.git' },
   init_options = { formatter = 'auto' },
+  ---@param client vim.lsp.Client
+  ---@param config vim.lsp.ClientConfig
   reuse_client = function(client, config)
     config.cmd_cwd = config.root_dir
     return client.config.cmd_cwd == config.cmd_cwd
