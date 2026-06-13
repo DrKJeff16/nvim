2750783299e5799b9eb2adf237860843d1736c72
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-06-13 17:51:03 -0600

    ci: create LSP server creator script
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ scripts/new-lsp-server.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Create new plugin from template
+#
+
+[[ $# -eq 0 ]] && exit 1
+
+set -o pipefail
+
+TARGET_DIR="lua/config/lsp/servers"
+TEMPLATE="lsp_template.lua"
+F_EXT="$(echo "${TARGET}" | rev | cut -d '.' -f1 | rev)"
+TARGET="$1"
+
+[[ "$F_EXT" != "lua" ]] && TARGET+=".lua"
+
+if [[ -f "${TARGET_DIR}/${TARGET}" ]]; then
+    cp -vi "scripts/${TEMPLATE}" "${TARGET_DIR}/${TARGET}" || exit 1
+else
+    cp -v "scripts/${TEMPLATE}" "${TARGET_DIR}/${TARGET}" || exit 1
+fi
+exit 0
