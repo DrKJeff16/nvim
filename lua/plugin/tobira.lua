47493ae1f8e285d3be269a3cbc20316879eab8c2
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-07-21 17:20:47 -0600

    feat(plugin.tobira): add setup options
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ lua/plugin/tobira.lua
@@ -0,0 +1,9 @@
+---@module 'lazy'
+return { ---@type LazySpec
+  'kamegoro/tobira.nvim',
+  dev = true,
+  event = 'VeryLazy',
+  version = false,
+  opts = {},
+}
+-- vim: set ts=2 sts=2 sw=2 et ai si sta:
