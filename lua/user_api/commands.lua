cc6264437e64986bd3856b3e7b1a07074db9927a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-08-10 01:32:03 -0600

    chore(api): maintenance
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user_api/commands.lua
+++ lua/user_api/commands.lua
@@ -3,7 +3,6 @@
 ---@field [2]? vim.api.keyset.user_command
 
 local INFO = vim.log.levels.INFO
-local ERROR = vim.log.levels.ERROR
 local validate = require('user_api.check').validate
 
 ---@class User.Commands
@@ -13,29 +12,34 @@ local M = {}
 ---@param bang? boolean
 ---@param nargs? integer|'+'|'?'|'*'
 ---@param complete? string|fun(a: string, b: string, c: integer): items: string[]
+---@param bar? boolean
 ---@return vim.api.keyset.user_command opts
-function M.desc(txt, bang, nargs, complete)
+function M.desc(txt, bang, nargs, complete, bar)
   validate({
     txt = { txt, { 'string', 'nil' }, true },
     bang = { bang, { 'boolean', 'nil' }, true },
     nargs = { nargs, { 'number', 'string', 'nil' }, true },
     complete = { complete, { 'string', 'function', 'nil' }, true },
+    bar = { bar, { 'boolean', 'nil' }, true },
   })
   txt = (txt and txt ~= '') and txt or nil
   if bang == nil then
     bang = false
   end
+  if bar == nil then
+    bar = false
+  end
 
-  local opts = { bang = bang, force = true } ---@type vim.api.keyset.user_command
+  local opts = { bang = bang, bar = bar, force = true } ---@type vim.api.keyset.user_command
   if txt and txt ~= '' then
     opts.desc = txt
   end
   if nargs then
-    if type(nargs) == 'number' and require('user_api.check').is_int(nargs) and nargs < 0 then
-      error(('`nargs` is not a valid integer: `%s`'):format(nargs), ERROR)
+    if type(nargs) == 'number' and require('user_api.check').is_int(nargs) and nargs <= 0 then
+      error(('`nargs` is not a valid integer: `%s`'):format(nargs))
     end
     if type(nargs) == 'string' and not vim.list_contains({ '+', '*', '?' }, nargs) then
-      error(("`nargs` is not a valid string (`'*'`, `'?'`, `'+'`): `%s`"):format(nargs), ERROR)
+      error(("`nargs` is not a valid string (`'*'`, `'?'`, `'+'`): `%s`"):format(nargs))
     end
     opts.nargs = nargs
   end
@@ -46,6 +50,69 @@ function M.desc(txt, bang, nargs, complete)
 end
 
 local commands = { ---@type table<string, User.Commands.CmdSpec>
+  Current = {
+    function(ctx)
+      local curr = { ---@type { buffer: integer, window: integer, tabpage: integer }
+        buffer = vim.api.nvim_get_current_buf(),
+        window = vim.api.nvim_get_current_win(),
+        tabpage = vim.api.nvim_get_current_tabpage(),
+      }
+      if vim.tbl_isempty(ctx.fargs) then
+        vim.notify(('buffer: %s\nwindow: %s\ntabpage %s'):format(curr.buffer, curr.window, curr.tabpage), INFO)
+        return
+      end
+
+      local arg = ctx.fargs[1] ---@type 'buffer'|'buf'|'window'|'win'|'tab'|'tabpage'
+      if not vim.list_contains({ 'buffer', 'buf', 'window', 'win', 'tabpage', 'tab' }, arg) then
+        vim.notify(('(:Current) - Invalid argument `%s`!'):format(arg), vim.log.levels.ERROR)
+        return
+      end
+
+      local msg, title = '', ''
+      if vim.list_contains({ 'buffer', 'buf' }, arg) then
+        msg = ('%d'):format(curr.buffer)
+        title = 'Current Buffer'
+      elseif vim.list_contains({ 'window', 'win' }, arg) then
+        msg = ('%d'):format(curr.window)
+        title = 'Current Window'
+      elseif vim.list_contains({ 'tabpage', 'tab' }, arg) then
+        msg = ('%d'):format(curr.tabpage)
+        title = 'Current Tabpage'
+      end
+
+      vim.notify(msg, INFO, { title = title })
+    end,
+    M.desc('', false, '?', function(_, lead)
+      local args = vim.split(lead, '%s+', { trimempty = false })
+      table.remove(args, 1)
+
+      if #args ~= 1 then
+        return {}
+      end
+
+      local comp = {} ---@type string[]
+      for _, choice in ipairs({ 'buf', 'buffer', 'win', 'window', 'tab', 'tabpage' }) do
+        if vim.startswith(choice, args[1]) then
+          table.insert(comp, choice)
+        end
+      end
+      return comp
+    end, true),
+  },
+  DeleteInactiveBuffers = {
+    function(ctx)
+      for _, buf in ipairs(vim.fn.getbufinfo()) do
+        if vim.tbl_isempty(buf.windows) and buf.listed == 1 and buf.loaded == 1 then
+          ctx.bang = true
+          vim.cmd.bdelete({ buf.bufnr, bang = true })
+        end
+      end
+      if ctx.bang then
+        vim.notify('Deleted inactive buffers.', INFO)
+      end
+    end,
+    M.desc('Delete listed unmodified buffers out of window', true),
+  },
   ErrQuit = {
     function()
       vim.api.nvim_exec_autocmds('VimLeavePre', {})
@@ -96,70 +163,6 @@ local commands = { ---@type table<string, User.Commands.CmdSpec>
       vim.cmd.wincmd('=')
     end,
     M.desc('Redirect command output to scratch buffer', true, '+', 'command'),
-  },
-
-  Current = {
-    function(ctx)
-      local curr = { ---@type { buffer: integer, window: integer, tabpage: integer }
-        buffer = vim.api.nvim_get_current_buf(),
-        window = vim.api.nvim_get_current_win(),
-        tabpage = vim.api.nvim_get_current_tabpage(),
-      }
-      if vim.tbl_isempty(ctx.fargs) then
-        vim.notify(('buffer: %s\nwindow: %s\ntabpage %s'):format(curr.buffer, curr.window, curr.tabpage), INFO)
-        return
-      end
-
-      local arg = ctx.fargs[1] ---@type 'buffer'|'buf'|'window'|'win'|'tab'|'tabpage'
-      if not vim.list_contains({ 'buffer', 'buf', 'window', 'win', 'tabpage', 'tab' }, arg) then
-        vim.notify(('(:Current) - Invalid argument `%s`!'):format(arg), ERROR)
-        return
-      end
-
-      local msg, title = '', ''
-      if vim.list_contains({ 'buffer', 'buf' }, arg) then
-        msg = ('%d'):format(curr.buffer)
-        title = 'Current Buffer'
-      elseif vim.list_contains({ 'window', 'win' }, arg) then
-        msg = ('%d'):format(curr.window)
-        title = 'Current Window'
-      elseif vim.list_contains({ 'tabpage', 'tab' }, arg) then
-        msg = ('%d'):format(curr.tabpage)
-        title = 'Current Tabpage'
-      end
-
-      vim.notify(msg, INFO, { title = title })
-    end,
-    M.desc('', false, '?', function(_, lead)
-      local args = vim.split(lead, '%s+', { trimempty = false })
-      table.remove(args, 1)
-
-      if #args ~= 1 then
-        return {}
-      end
-
-      local comp = {} ---@type string[]
-      for _, choice in ipairs({ 'buf', 'buffer', 'win', 'window', 'tab', 'tabpage' }) do
-        if vim.startswith(choice, args[1]) then
-          table.insert(comp, choice)
-        end
-      end
-      return comp
-    end),
-  },
-  DeleteInactiveBuffers = {
-    function(ctx)
-      for _, buf in ipairs(vim.fn.getbufinfo()) do
-        if vim.tbl_isempty(buf.windows) and buf.listed == 1 and buf.loaded == 1 then
-          ctx.bang = true
-          vim.cmd.bdelete({ buf.bufnr, bang = true })
-        end
-      end
-      if ctx.bang then
-        vim.notify('Deleted inactive buffers.', vim.log.levels.INFO)
-      end
-    end,
-    M.desc('Delete listed unmodified buffers out of window', true),
   },
 }
 
